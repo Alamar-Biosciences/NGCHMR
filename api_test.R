5d16f438da406185642a6fcdbd9ce1231b7d412d
Dwight Kuo <dkuo@alamarbio.comn>
2021-11-02 18:14:36 -0700

    try changing host from 0.0.0.0 to 127.0.01

--- api_test.R
+++ api_test.R
@@ -4,7 +4,7 @@ library(httr)
 library(jsonlite)
 seed=TRUE
 options(stringsAsFactors=F)
-url <- "http://127.0.0.1:8000"
+url <- "http://localhost:8000"
 data <- read.table("test_data.txt", sep="\t", header=T, row.names=1)
 raw.result <- POST(url=url, 
                    path="ngchm", 
