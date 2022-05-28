7f9be81050a24838f8e930fa3d28ca839f046cae
Dwight Kuo <dkuo@alamarbio.comn>
2022-05-27 21:13:39 -0700

    change tests to use new XML input

--- run_tests.sh
+++ run_tests.sh
@@ -9,8 +9,13 @@ PID=$!
 trap "kill $PID" EXIT;
 sleep 5
 
-cmd=`R --no-save < api_test.R`
-#cmd=`curl -v --form "data=@test_data.txt" --form "bcodeA=@bcodeA.txt" --form "bcodeB=@bcodeB.txt" http://localhost:8000/ngchm -o out1.ngchm` 
+#cmd=`R --no-save < api_test.R`
+#cmd=`curl -v --form "data=@test_data.txt" --form "bcodeA=@bcodeA.txt" --form "bcodeB=@bcodeB.txt" http://localhost:8000/ngchm -o out1.ngchm` ;
+cmd=`curl -v --form "data=@out_seqRepP.xml" --form "bcodeA=@20220325_BarcodeA.txt" --form "bcodeB=@20220325_BarcodeB.txt" http://localhost:8000/ngchm -o out1.ngchm` ;
+size=`stat out1.ngchm -c %B`;
+if (( size < 1024)); then
+  exit 1
+fi
 echo "$cmd"
 if [[ $cmd == *"Wrote results to"* ]]; then
   echo "Test succesful!...."
