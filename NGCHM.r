c4567232d69d729f5edc26612f7180f6f856c782
dwightkuo <dwightkuo@gmail.com>
2023-02-03 17:23:14 -0800

    add options to change clustering / agglomertion methods in NGCHM and fix swagger

--- NGCHM.r
+++ NGCHM.r
@@ -35,82 +35,79 @@ function(req){
 #
 # @param req JSON string, extract data matrix 
 # @return Returns ngchm file
-#* @serializer contentType list(type="application/octet-stream")
+#* @param data:file
+#* @param method
+#* @param keepControls
+#* @param bcodeBin:file
+#* @param bcodeAin:file
+#* @param IPC
+#* @param NC
+#* @param rowDist
+#* @param colDist
+#* @param rowAgglom
+#* @param colAgglom
 #* @post /ngchm
-function(req){
+ngchm <- function(data, method="IC", keepControls=FALSE, bcodeBin="", bcodeAin="", 
+                  IPC=c('InterPlateControl'), NC=c('NegativeControl'), IC=c('mCherry'),
+                  rowDist='euclidean', colDist='euclidean', rowAgglom='average', colAgglom='average'){
   future({
-  parsed <- parse_multipart(req)
-  methodName = "IC";
-  keepControls <- FALSE;
-  if (is.element("keepControls", names(parsed))){
-    keepControls <- TRUE;
+  root <- xml_root(read_xml(toString(data)))
+
+  # Read BarcodeAs
+  x <- NULL; y <- NULL;
+  for (barcodeA in xml_find_all(root, ".//BarcodeA")){
+    for (bcodeA in xml_find_all(barcodeA, ".//Barcode")){
+      x <- if(is.null(x)) xml_attrs(bcodeA) else rbind(x, xml_attrs(bcodeA))
+      y <- if(is.null(y)) xml_text(bcodeA) else  rbind(y, xml_text(bcodeA))
+    }
+    colnames(y) <- "Target"
+    BarcodeA <- if (ncol(x) == 1) cbind(x, y) else cbind(x[, 1], y, x[, 2:ncol(x)])
+    colnames(BarcodeA)[1] <- "name"
+    rownames(BarcodeA) <- NULL;
+    BarcodeA <- as.data.frame(BarcodeA)
   }
-  if (is.element("method", names(parsed))){
-    methodName <- parsed$method
-  } 
-  if (is.element("data", names(parsed))){
-    data <- read_xml(parsed$data$datapath)
-    root <- xml_root(data)
 
-    # Read BarcodeAs
-    x <- NULL; y <- NULL;
-    for (barcodeA in xml_find_all(root, ".//BarcodeA")){
-      for (bcodeA in xml_find_all(barcodeA, ".//Barcode")){
-        x <- if(is.null(x)) xml_attrs(bcodeA) else rbind(x, xml_attrs(bcodeA))
-        y <- if(is.null(y)) xml_text(bcodeA) else  rbind(y, xml_text(bcodeA))
+  # Read BarcodeBs
+  x <- NULL; y <- NULL;
+  for (barcodeB in xml_find_all(root, ".//BarcodeB")){
+    for (bcodeB in xml_find_all(barcodeB, ".//Barcode")){
+      if (keepControls || (!grepl(paste(NC, collapse="|"), xml_text(bcodeB)) && !grepl(paste(IPC, collapse="|"), xml_text(bcodeB)))){
+        x <- if(is.null(x)) xml_attrs(bcodeB) else rbind(x, xml_attrs(bcodeB))
+        y <- if(is.null(y)) xml_text(bcodeB)  else rbind(y, xml_text(bcodeB))
       }
-      colnames(y) <- "Target"
-      BarcodeA <- if (ncol(x) == 1) cbind(x, y) else cbind(x[, 1], y, x[, 2:ncol(x)])
-      colnames(BarcodeA)[1] <- "name"
-      rownames(BarcodeA) <- NULL;
-      BarcodeA <- as.data.frame(BarcodeA)
     }
+    colnames(y) <- "Sample"
+    BarcodeB <- if (ncol(x) == 1) cbind(x, y) else cbind(x[, 1], y, x[, 2:ncol(x)])
+    colnames(BarcodeB)[1] <- "name"
+    rownames(BarcodeB) <- NULL;
+    BarcodeB <- as.data.frame(BarcodeB)
+  }
 
-    # Read BarcodeBs
-    x <- NULL; y <- NULL;
-    for (barcodeB in xml_find_all(root, ".//BarcodeB")){
-      for (bcodeB in xml_find_all(barcodeB, ".//Barcode")){
-        if (keepControls || (!grepl("NegativeControl", xml_text(bcodeB)) && !grepl("InterPlateControl", xml_text(bcodeB)))){
-          x <- if(is.null(x)) xml_attrs(bcodeB) else rbind(x, xml_attrs(bcodeB))
-          y <- if(is.null(y)) xml_text(bcodeB)  else rbind(y, xml_text(bcodeB))
-        }
-      }
-      colnames(y) <- "Sample"
-      BarcodeB <- if (ncol(x) == 1) cbind(x, y) else cbind(x[, 1], y, x[, 2:ncol(x)])
-      colnames(BarcodeB)[1] <- "name"
-      rownames(BarcodeB) <- NULL;
-      BarcodeB <- as.data.frame(BarcodeB)
-    }
-
-    # Read the actual data for the heatmap
-    storage = as.data.frame(matrix(nrow=length(unique(BarcodeA$Target)), ncol=length(unique(BarcodeB$Sample))))
-    colnames(storage) = unique(BarcodeB$Sample)
-    rownames(storage) = unique(BarcodeA$Target)
-    for (sample in xml_find_all(root, ".//Sample")){
-      col <- which(colnames(storage) == xml_attr(sample, 'name'))
-      for (combined in xml_find_all(sample, ".//Combined")){ # "Combined" - Sample, or "Replicate"
-        for (method in xml_find_all(combined, ".//Method")){
-          if(xml_attr(method, 'name') == methodName){ # "raw", "IC", or "TC"
-            for(target in xml_find_all(method, ".//Target")){
-              targetName <- BarcodeA[which(BarcodeA$name == xml_attr(target, 'name')), ]$Target
-              row <- which(rownames(storage) == targetName)
-              storage[row, col] <- as.numeric(xml_text(target)) 
-            }
+  # Read the actual data for the heatmap
+  storage = as.data.frame(matrix(nrow=length(unique(BarcodeA$Target)), ncol=length(unique(BarcodeB$Sample))))
+  colnames(storage) = unique(BarcodeB$Sample)
+  rownames(storage) = unique(BarcodeA$Target)
+  for (sample in xml_find_all(root, ".//Sample")){
+    col <- which(colnames(storage) == xml_attr(sample, 'name'))
+    for (combined in xml_find_all(sample, ".//Combined")){ # "Combined" - Sample, or "Replicate"
+      for (method2 in xml_find_all(combined, ".//Method")){
+        if(xml_attr(method2, 'name') == method){ # "raw", "IC", or "TC"
+          for(target in xml_find_all(method2, ".//Target")){
+            targetName <- BarcodeA[which(BarcodeA$name == xml_attr(target, 'name')), ]$Target
+            row <- which(rownames(storage) == targetName)
+            storage[row, col] <- as.numeric(xml_text(target)) 
           }
         }
       }
     }
   }
-  else{
-    write("Error: No data file provided, no NGCHM generated!",stdout())
-  }
 
   # Create the heatmap
-  hm <- chmNew('temp', as.matrix(storage), rowDist='euclidean', colDist='euclidean', rowAgglom='average', colAgglom='average')
+  hm <- chmNew('temp', as.matrix(storage), rowDist=rowDist, colDist=colDist, rowAgglom=rowAgglom, colAgglom=colAgglom)
 
   # BarcodeB / Targets / Covariates processing 
-  if (is.element("bcodeB", names(parsed))){
-    bcodeB <- read.table(parsed$bcodeB$datapath, sep='\t', comment.char='&', header=T, row.names=1, na.strings=c())
+  if (bcodeBin != ""){
+    bcodeB <- read.table(text=toString(bcodeBin), sep='\t', comment.char='&', header=T, row.names=1, na.strings=c())
     name <- colnames(bcodeB)
   }
   else{
@@ -148,8 +145,8 @@ function(req){
   }
  
   # BarcodeA / Targets / Covariates processing 
-  if (is.element("bcodeA", names(parsed))){
-    bcodeA <- read.table(parsed$bcodeA$datapath, sep='\t', comment.char='&', header=T, row.names=1, na.strings=c()) 
+  if (bcodeAin != ""){
+    bcodeA <- read.table(text=toString(bcodeAin), sep='\t', comment.char='&', header=T, row.names=1, na.strings=c()) 
     name <- names(bcodeA)
     
   }
