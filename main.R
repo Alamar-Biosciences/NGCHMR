327e8e788413b3cda9461e034739a7831cfe0be0
dwightkuo <dwightkuo@gmail.com>
2023-02-06 11:49:21 -0800

    use promises for async API

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 library(plumber)
 library(future)
-plan(multicore, workers=2)
+plan(cluster, workers=2) # do not use multicore (possible garbage collection issue)
 args = commandArgs(trailingOnly=T)
 PORT <- if (length(args) == 0 ) 8000 else args[1]
 if (file.exists("/workingDir")){
